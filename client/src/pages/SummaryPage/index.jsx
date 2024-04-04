516c963f67b2f685355d9f16632313dbb1059612
jchankim95 <kimjchan95@gmail.com>
2024-04-05 00:44:10 +0900

    feat : SummaryPage에서 상품, 옵션 리스트 나올 수 있게 수정

--- client/src/pages/SummaryPage/index.jsx
+++ client/src/pages/SummaryPage/index.jsx
@@ -1,10 +1,42 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
+import { OrderConext } from "../../context/OrderContext";
 
-const SummaryPage = () => {
+const SummaryPage = ({setStep}) => {
   const [checked, setChecked] = useState(false);
+  const [orderDetails] = useContext(OrderConext);
+
+  const productArray = Array.from(orderDetails.products); //얕게 복사
+  const productList = productArray.map(([key, value])=>
+    <li key={key}>
+      {value} {key}
+    </li>
+  );
+
+  const hasOptions = orderDetails.options.size > 0;
+  let optionDisplay = null;
+  if(hasOptions){
+    const optionsArray = Array.from(orderDetails.options.keys());
+    const optionList = optionsArray.map((key) => <li key={key}>{key}</li>)
+
+    optionDisplay = (
+      <>
+        <h2>옵션 : {orderDetails.totals.options}</h2>
+        <ol>{optionList}</ol>
+      </>
+    )
+  }
+
+  const handleSubmit = ()=>{
+    setStep(2);
+  }
+
   return (
     <div>
-      <form>
+      <h1>주문 확인</h1>
+      <h2>여행 상품 : {orderDetails.totals.products}</h2>
+      <ul>{productList}</ul>
+      {optionDisplay}
+      <form onSubmit={handleSubmit}>
         <input
           type="checkbox"
           checked={checked}
